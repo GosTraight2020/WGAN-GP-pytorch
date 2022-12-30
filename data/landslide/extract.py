b3596e945d9697aac4fc5ead8d611ae9f91ff51b
GosTraight2020 <shanlin.liu@intel.com>
2022-12-30 15:05:00 +0000

    [Update] Add extract.py to classify the data

--- data/landslide/extract.py
+++ data/landslide/extract.py
@@ -5,9 +5,18 @@ import h5py
 import scipy.misc
 import shutil 
 
-data_dir = '/exp/WGAN-GP-pytorch/data/landslide/'
-img_dir = os.path.join(data_dir, 'img')
+data_dir = './data/landslide/'
+img_dir = os.path.join(data_dir, 'image')
 mask_dir = os.path.join(data_dir, 'mask')
+image_dir_yes = os.path.join(img_dir, 'yes')
+image_dir_no = os.path.join(img_dir, 'no')
+mask_dir_yes = os.path.join(mask_dir, 'yes')
+mask_dir_no = os.path.join(mask_dir, 'no')
+
+for d in [image_dir_yes, image_dir_no, mask_dir_yes, mask_dir_no]:
+    if not os.path.exists(d):
+        os.makedirs(d, exist_ok=True)
+
 file_pattern = os.path.join(img_dir, '*.h5')
 file_list = sorted(glob.glob(file_pattern))
 
@@ -23,15 +32,15 @@ for img_file in file_list:
         # non-landslide
         if mask[mask==1].size == 0:
             img_from = img_file
-            img_to = os.path.join(img_dir, 'no', img_name)
+            img_to = os.path.join(image_dir_no, img_name)
             mask_from = mask_file
-            mask_to = os.path.join(mask_dir, 'no', mask_name)
+            mask_to = os.path.join(mask_dir_no, mask_name)
             print('{}, img_from: {}, img_to:{}, mask_from:{}, mask_to:{}'.format(mask[mask==1].size, img_from, img_to, mask_from, mask_to))
         else:
             img_from = img_file
-            img_to = os.path.join(img_dir, 'yes', img_name)
+            img_to = os.path.join(image_dir_yes, img_name)
             mask_from = mask_file
-            mask_to = os.path.join(mask_dir, 'yes', mask_name)
+            mask_to = os.path.join(mask_dir_yes, mask_name)
             print('{}, img_from: {}, img_to:{}, mask_from:{}, mask_to:{}'.format(mask[mask==1].size, img_from, img_to, mask_from, mask_to))
         shutil.move(img_from, img_to)
         shutil.move(mask_from, mask_to)
